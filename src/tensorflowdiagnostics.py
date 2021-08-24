88473a50d37b37d33e7b4ebad14b290611f62142
Alexey Izmailov <alizma@brown.edu>
2021-08-24 18:07:20 -0400

    added tensorflow local diagnostics

---
+++ src/tensorflowdiagnostics.py
@@ -0,0 +1,17 @@
+import importlib
+import os
+
+if __name__ == '__main__':
+    print('*** Starting TensorFlow diagnostics')
+    os.environ['TF_CPP_MIN_VLOG_LEVEL'] = '0'
+    
+    tensorflow  = importlib.import_module('tensorflow')
+    device_lib  = importlib.import_module('tensorflow.python.client.device_lib')
+
+    print('\n*** GPU available: {}'.format('yes' if tensorflow.test.is_gpu_available() else 'no'))
+    print('\n*** Devices found by TensorFlow:\n{}'.format(
+        '\n * '.join(list(map(str, device_lib.list_local_devices())))
+    ))
+    gpu_device = tensorflow.test.gpu_device_name()
+    if gpu_device:
+        print('\n*** GPU device name: {}'.format(gpu_device))
